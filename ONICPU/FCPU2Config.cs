2beb07fc8192bdc91682094035a95a668e6538af
快乐的梦鱼 <32213395+imengyu@users.noreply.github.com>
2024-06-09 11:18:04 +0800

    Add 2pin cpu

---
+++ ONICPU/FCPU2Config.cs
@@ -0,0 +1,25 @@
+﻿namespace ONICPU
+{
+  public class FCPU2Config : FCPU4Config
+  {
+    public new const string ID = "FCPU2";
+
+    protected override string GetID()
+    {
+      return ID;
+    }
+    protected override string GetAnim()
+    {
+      return "fcpu2_kanim";
+    }
+    protected override int GetH()
+    {
+      return 2;
+    }
+    protected override void OnFCpuAdded(FCPU component)
+    {
+      component.portCount = 2;
+      component.CPUType = FCPU.FCPUType.AssemblyCode;
+    }
+  }
+}
---
+++ ONICPU/FCPU2JSConfig.cs
@@ -0,0 +1,21 @@
+﻿namespace ONICPU
+{
+  public class FCPU2JSConfig : FCPU2Config
+  {
+    public new const string ID = "FCPU2JS";
+
+    protected override string GetID()
+    {
+      return ID;
+    }
+    protected override string GetAnim()
+    {
+      return "fcpu2_kanim";
+    }
+    protected override void OnFCpuAdded(FCPU component)
+    {
+      component.portCount = 2;
+      component.CPUType = FCPU.FCPUType.JavaScript;
+    }
+  }
+}
