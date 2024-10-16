d033c5450b7bb5d5379414ec6029ba3f672db77f
yuanze31 <1538981620@qq.com>
2024-10-16 15:14:30 +0800

    Rebuild ReadMe

---
+++ demo/cycle.counter/main.js
@@ -0,0 +1,22 @@
+﻿var previousP0 = null;
+function start() {
+    io.P3 = storage.savedNowDays;
+    if (storage.savedNowDays == NaN) {
+        storage.savedNowDays = 31;
+    }
+}
+
+function stop() {}
+
+function tick() {
+    var currentP0 = io.P0;
+
+    if (previousP0 != null) {
+        if (previousP0 >= 99 && currentP0 <= 1) {
+            storage.savedNowDays = storage.savedNowDays + 1;
+            __log("days:" + storage.savedNowDays);
+        }
+    }
+    previousP0 = currentP0;
+    io.P2 = storage.savedNowDays;
+}
